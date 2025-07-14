ad97da51f2f076bb484eca913808f1433f2e7571
leitolarreta <leito.larreta@gmail.com>
2025-07-14 20:41:26 -0300

    Delete app/composeApp/src/commonMain/kotlin/ext/AppConfig.kt

--- app/composeApp/src/commonMain/kotlin/ext/AppConfig.kt
+++
@@ -1,9 +0,0 @@
-package ext
-
-import ar.com.intrale.BuildKonfig
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-
-@OptIn(ExperimentalResourceApi::class)
-object AppConfig {
-    val baseUrl: String = BuildKonfig.BASE_URL
-}
