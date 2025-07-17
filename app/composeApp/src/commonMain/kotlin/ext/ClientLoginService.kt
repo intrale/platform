e5c8e11b405054e48f0e86bcc62e6f92ac514b3d
leitolarreta <leito.larreta@gmail.com>
2025-07-17 18:37:35 -0300

    refactor: update login response handling and improve error logging

--- app/composeApp/src/commonMain/kotlin/ext/ClientLoginService.kt
+++ app/composeApp/src/commonMain/kotlin/ext/ClientLoginService.kt
@@ -1,46 +1,50 @@
 package ext
 
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.plugins.ClientRequestException
 import io.ktor.client.request.headers
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import ar.com.intrale.BuildKonfig
-import io.ktor.utils.io.InternalAPI
-import kotlinx.serialization.Serializable
 import org.kodein.log.LoggerFactory
 import org.kodein.log.newLogger
+import io.ktor.client.statement.bodyAsText
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.isSuccess
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.Serializable
 
 class ClientLoginService(val httpClient: HttpClient) : CommLoginService {
 
     private val logger = LoggerFactory.default.newLogger<ClientLoginService>()
 
-    @OptIn(InternalAPI::class)
     override suspend fun execute(user: String, password: String): Result<LoginResponse> {
         return try {
-            val response: LoginResponse =
+            val response: HttpResponse =
                 httpClient.post("${BuildKonfig.BASE_URL}${BuildKonfig.BUSINESS}/signin") {
                     headers {
-
+                        // Agregá headers si los necesitás
                     }
                     setBody(LoginRequest(user, password))
-                }.body()
+                }
 
-            logger.debug {
-                "response body:"  + response
+            val bodyText = response.bodyAsText()
+
+            if (response.status.isSuccess()) {
+                val loginResponse = Json.decodeFromString(LoginResponse.serializer(), bodyText)
+                logger.debug { "response body: $loginResponse" }
+                Result.success(loginResponse)
+            } else {
+                logger.debug { "login failed with status ${response.status}: $bodyText" }
+                Result.failure(Exception("Login fallido: $bodyText"))
             }
 
-            Result.success(response)
-        } catch (e: ClientRequestException) {
-            logger.error { "client error: ${e.message}" }
-            Result.failure(e)
         } catch (e: Exception) {
             logger.error { "login error: ${e.message}" }
             Result.failure(e)
         }
     }
 }
+
 
 @Serializable
 data class LoginRequest(val email:String, val password: String)
