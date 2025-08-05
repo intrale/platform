7eb8673075fcbd5eebe61f58796d6417cf5994d9
leitolarreta <leito.larreta@gmail.com>
2025-08-05 16:06:00 -0300

    feat: agrega loggers en enrutadores ui/ro

--- app/composeApp/src/commonMain/kotlin/ui/ro/Router.kt
+++ app/composeApp/src/commonMain/kotlin/ui/ro/Router.kt
@@ -6,8 +6,12 @@ import androidx.compose.runtime.State
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import ui.sc.Screen
+import org.kodein.log.LoggerFactory
+import org.kodein.log.newLogger
 
 abstract class Router (var navigator: NavHostController){
+
+    protected val logger = LoggerFactory.default.newLogger<Router>()
 
     @Composable
     abstract fun routes()
