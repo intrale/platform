8460ed636185d84be017fdf5be0dceb755758019
leitolarreta <leito.larreta@gmail.com>
2025-09-18 09:21:13 -0300

    Corrige errores de compilación en Compose

--- app/composeApp/src/commonMain/kotlin/ui/sc/ReviewBusinessScreen.kt
+++ app/composeApp/src/commonMain/kotlin/ui/sc/ReviewBusinessScreen.kt
@@ -1,7 +1,6 @@
 package ui.sc
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Card
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
