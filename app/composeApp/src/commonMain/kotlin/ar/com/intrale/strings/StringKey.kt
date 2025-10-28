31661ae1f29beb4aa239210303d51960f6153531
leitolarreta <leito.larreta@gmail.com>
2025-10-28 14:32:01 -0300

    Add string localization support with default catalogs and brand handling

---
+++ app/composeApp/src/commonMain/kotlin/ar/com/intrale/strings/StringKey.kt
@@ -0,0 +1,9 @@
+package ar.com.intrale.strings
+
+/** Claves tipadas. Podés empezar con pocas y después crecer. */
+enum class StringKey {
+    App_Name,
+    Login_Title,
+    Login_Button,
+    Error_Generic,
+}
