129c8c6fbd4b1af6a3741f50f99a2aa2fc79c604
leitolarreta <leito.larreta@gmail.com>
2025-10-29 09:22:23 -0300

    feat: add forbidden strings processor and type-safe string keys for localization

---
+++ app/composeApp/src/commonMain/kotlin/ar/com/intrale/strings/catalog/DefaultCatalog_es.kt
@@ -0,0 +1,11 @@
+package ar.com.intrale.strings.catalog
+
+import ar.com.intrale.strings.model.MessageKey
+
+internal val DefaultCatalog_es: Map<MessageKey, String> = mapOf(
+    MessageKey.app_name      to "Intrale",
+    MessageKey.login_title   to "Iniciar sesión",
+    MessageKey.login_subtitle to "Ingresá con tu cuenta",
+    MessageKey.login_button  to "Continuar",
+    MessageKey.error_generic to "Ocurrió un error. Intenta nuevamente.",
+)
